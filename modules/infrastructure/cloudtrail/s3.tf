43ee8b623f2b21c2fd9cb071b5d5f47d40bd309e
TaeHyeong Kim <rahu2000@naver.com>
2023-07-25 02:31:33 +0000

    modify: Notice: S3 - Changes to Defaults (Block Public Access and ACL's) https://stackoverflow.com/questions/76049290/error-accesscontrollistnotsupported-when-trying-to-create-a-bucket-acl-in-aws
    Signed-off-by: TaeHyeong Kim <rahu2000@naver.com>

--- modules/infrastructure/cloudtrail/s3.tf
+++ modules/infrastructure/cloudtrail/s3.tf
@@ -35,10 +35,18 @@ resource "aws_s3_bucket_lifecycle_configuration" "cloudtrail" {
 
 
 resource "aws_s3_bucket_acl" "cloudtrail" {
-  bucket = aws_s3_bucket.cloudtrail.id
-  acl    = "private"
+  bucket     = aws_s3_bucket.cloudtrail.id
+  acl        = "private"
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
 
+# Resource to avoid error "AccessControlListNotSupported: The bucket does not allow ACLs"
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.cloudtrail.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
 
 # --------------------------
 # iam, acl
